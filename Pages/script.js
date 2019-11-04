8a26a727206e30c034a6d3e60c0cc5fd9020ddac
643283 <54897358+643283@users.noreply.github.com>
2019-11-04 11:00:43 -0500

    assignment 2

--- Pages/script.js
+++ Pages/script.js
@@ -1,3 +1,37 @@
 function hello(){
 	alert("hello");
 }
+
+function triangle(){
+	let A1 = parseInt(prompt ("enter the first angle"))
+	if (A1 > 0 && A1 <179) {
+	let A2 = parseInt(prompt ("enter the second angle"))
+	let A3 = 180 - (A1 + A2) 
+	alert (A3)
+	}
+	else{alert("only type numbers 1-178!")}
+}
+
+function year(){
+	let inputyear =  parseInt(prompt ("enter the year"))
+	if (inputyear > 0){
+	let century = Math.ceil(inputyear / 100)
+	alert (century)
+	}
+	else{alert("enter only numbers!")}
+}
+
+function tenfour(){
+	for (let x = 1; x < 101; x++){
+		if (x % 4 == 0 && x % 10 == 0){
+		console.log ("tenfour");
+		}
+	else if (x % 4 == 0){
+		console.log ("Four");
+	}
+	else if (x % 10 == 0) {
+		console.log ("Ten")
+	}
+	else {console.log (x)}
+	}
+}
